d675f601a70c02581c574cd066925d98229b42a3
Rahul Agrawal Bejarano <rahul.agrawal.b@gmail.com>
2022-07-20 16:47:18 -0400

    NAs appear in columns and structure allows this now

--- create_sql_tables/create_countries.sql
+++ create_sql_tables/create_countries.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE countries(
-    iso3c VARCHAR(3) NOT NULL PRIMARY KEY,
+    record_id SERIAL NOT NULL PRIMARY KEY,
+    iso3c VARCHAR(3),
     iso2c VARCHAR(2),
     country_name VARCHAR(100),
     continent VARCHAR(100),
--- create_sql_tables/create_production.sql
+++ create_sql_tables/create_production.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE production(
   record_id SERIAL NOT NULL PRIMARY KEY,
-  iso3c VARCHAR(3) NOT NULL,
-  sciname VARCHAR(100) NOT NULL,
+  iso3c VARCHAR(3),
+  sciname VARCHAR(100),
   prod_method VARCHAR(12),
   environment VARCHAR(6),
   live_weight_t FLOAT,
