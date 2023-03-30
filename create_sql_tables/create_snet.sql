05e842045c885c4379d6e25a37b1ecb6da57aadc
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2023-03-30 13:46:57 -0400

    updated column order based on new snet pipeline

--- create_sql_tables/create_snet.sql
+++ create_sql_tables/create_snet.sql
@@ -2,14 +2,14 @@ CREATE TABLE snet(
     record_id SERIAL NOT NULL PRIMARY KEY,
     exporter_iso3c VARCHAR(3),
     importer_iso3c VARCHAR(3),
+    hs6 VARCHAR(6),
+    product_weight_t FLOAT,
     dom_source VARCHAR(100),
     source_country_iso3c VARCHAR(7),
-    hs6 VARCHAR(6),
     sciname VARCHAR(100),
     habitat VARCHAR(100),
     "method" VARCHAR(100),
-    product_weight_t FLOAT,
-    live_weight_t FLOAT,
     hs_version VARCHAR(4),
-    "year" INT
+    "year" INT,
+    live_weight_t FLOAT
 );
