e6f729c16628dee72910138fa29b946374a90cd3
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2023-07-05 10:56:39 -0400

    adding species identifiers, IUCN and CITES data to sciname metadata

--- create_sql_tables/create_sciname_metadata.sql
+++ create_sql_tables/create_sciname_metadata.sql
@@ -11,5 +11,11 @@ CREATE TABLE sciname(
     superclass VARCHAR(100),
     phylum VARCHAR(100),
     kingdom VARCHAR(100),
-    isscaap VARCHAR(100)
+    isscaap VARCHAR(100),
+    species_identifier VARCHAR(100),
+    species_taxonomic_code VARCHAR(100),
+    species_alpha3_code VARCHAR(100),
+    appendix VARCHAR(3),
+    cites_effective_date VARCHAR(100),
+    redlist_category VARCHAR(100)
 );
