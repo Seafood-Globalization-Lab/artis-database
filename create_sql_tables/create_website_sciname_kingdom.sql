eafa6e93dba026c45fb761933783967dc7041bd3
Althea Marks <amarks1@uw.edu>
2025-03-11 15:47:21 -0700

    Create create_website_sciname_kingdom.sql
    custom kingdom sciname table for website

---
+++ create_sql_tables/create_website_sciname_kingdom.sql
@@ -0,0 +1,14 @@
+CREATE TABLE scinamekingdom(
+    record_id SERIAL NOT NULL PRIMARY KEY,
+    sciname VARCHAR(100) NOT NULL,
+    common_name VARCHAR(255),
+    genus VARCHAR(100),
+    subfamily VARCHAR(100),
+    "family" VARCHAR(100),
+    "order" VARCHAR(100),
+    "class" VARCHAR(100),
+    superclass VARCHAR(100),
+    phylum VARCHAR(100),
+    kingdom VARCHAR(100),
+    isscaap VARCHAR(100)
+);
