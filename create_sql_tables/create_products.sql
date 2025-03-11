a8db0a1832ef20ed4c3d209734238595a052cdf9
Althea Marks <theamarks@gmail.com>
2025-03-11 13:12:51 -0700

    Update create_products.sql
    change classification field to hs_version

--- create_sql_tables/create_products.sql
+++ create_sql_tables/create_products.sql
@@ -4,7 +4,7 @@ CREATE TABLE products(
   hs6 VARCHAR(6) NOT NULL,
   description VARCHAR(255),
   parent VARCHAR(6),
-  classification VARCHAR(3),
+  hs_version VARCHAR(4),
   presentation VARCHAR(25),
   state VARCHAR(20)
 );
