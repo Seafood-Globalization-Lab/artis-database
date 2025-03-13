aee0df15b7c56f658ca9cd144c2a10ddc6469d1e
theamarks <theamarks@gmail.com>
2025-03-13 16:10:08 -0700

    update new summary artis tables by taxa rank

--- create_sql_tables/create_website_sciname_superclass.sql
+++ create_sql_tables/create_website_sciname_superclass.sql
@@ -1,14 +1,15 @@
-CREATE TABLE scinamesuperclass(
+CREATE TABLE snet(
     record_id SERIAL NOT NULL PRIMARY KEY,
-    sciname VARCHAR(100) NOT NULL,
-    common_name VARCHAR(255),
-    genus VARCHAR(100),
-    subfamily VARCHAR(100),
-    "family" VARCHAR(100),
-    "order" VARCHAR(100),
-    "class" VARCHAR(100),
+    importer_iso3c VARCHAR(3),
+    exporter_iso3c VARCHAR(3),
+    hs6 VARCHAR(6),
+    product_weight_t FLOAT,
+    dom_source VARCHAR(100),
+    source_country_iso3c VARCHAR(7),
     superclass VARCHAR(100),
-    phylum VARCHAR(100),
-    kingdom VARCHAR(100),
-    isscaap VARCHAR(100)
+    habitat VARCHAR(100),
+    "method" VARCHAR(100),
+    live_weight_t FLOAT,
+    hs_version VARCHAR(4),
+    "year" INT
 );
