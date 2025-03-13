92a07e5c266ed8c42f019be1f7039f8dc520af90
Althea Marks <amarks1@uw.edu>
2025-03-13 16:15:50 -0700

    Update create_website_sciname_order.sql

--- create_sql_tables/create_website_sciname_order.sql
+++ create_sql_tables/create_website_sciname_order.sql
@@ -6,7 +6,7 @@ CREATE TABLE artis_order(
     product_weight_t FLOAT,
     dom_source VARCHAR(100),
     source_country_iso3c VARCHAR(7),
-    order VARCHAR(100),
+    "order" VARCHAR(100),
     habitat VARCHAR(100),
     "method" VARCHAR(100),
     live_weight_t FLOAT,
